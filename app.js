771e27c6d0b863e36b49d49091c7d991c66fcc6a
SamiraSagadi <87983535+SamiraSagadi@users.noreply.github.com>
2021-08-13 11:18:43 +0100

    js for the C2 em

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('form');
 
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    const inbox = parseInt(document.querySelector('#inbox').value);
+    const spam = parseInt(document.querySelector('#spam').value);
+    const results = document.querySelector('#results');
+    
+    if((inbox === '') || (inbox < 0) || (isNaN(inbox))){
+        //NaN !== NaN
+        results.innerHTML = "Please provide a valid number of emails in the inbox folder";
+        
+    } else if (spam === '' || spam < 0 || isNaN(spam)){
+        results.innerHTML = "Please provide a valid number of emails in the spam folder (This can be zero!)";
+    } else {
+    //calculate CO2 emission
+    const CO2 = ((inbox*4) + (spam*0.3))
+
+    //display the results
+    results.innerHTML = "This is your CO2 emission : " + CO2 + "g"  }
+});
